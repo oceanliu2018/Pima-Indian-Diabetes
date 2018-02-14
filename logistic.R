08eb6370fd0fba89ef8e36141bd599bdbab98608
Ocean Liu <oceanliu95@gmail.com>
2018-02-14 18:10:34 -0500

    updates

--- logistic.R
+++ logistic.R
@@ -1,10 +1,17 @@
-pima = read.csv('pima.csv')
-# original data sets has missing values denoted by the zero
+# Load packages
+library(ROCR)
+library(caret)
 
+pima = read.csv('pima.csv')
+
+# original data sets has missing values denoted by the zero
+# exlude rows with zero in data frame excluding test variable
 clean.pima = pima[rowSums(pima[2:7] == 0) ==0,]
-#exludes rows with zero in data frame excluding test variable
 
 summary(clean.pima)
+
+
+
 
 log1 = glm(test~.,data = clean.pima,family = binomial)
 summary(log1)
@@ -12,13 +19,23 @@ summary(log1)
 log2 = glm(test~glucose+bmi+diabetes,data = clean.pima,family = binomial)
 summary(log2)
 
+# Compare the deviance of the two models
 anova(log1,log2,test = 'Chi')
 
-fitvalues = log2$fitted.values
-score.vec = seq(0, 1, by = 0.01)
-tpr.vec = numeric(101)
-fpr.vec = numeric(101)
-for (i in 1:101){
+# Add age to the reduced model
+log3 = glm(test~glucose+bmi+diabetes+age,data = clean.pima,family = binomial)
+summary(log3)
+
+# COmpare deviance of new reduced model
+anova(log1,log3,test = "Chi")
+
+
+# Compute Specificty and Sensitity
+fitvalues = log3$fitted.values
+score.vec = seq(0, 1, by = 0.001)
+tpr.vec = numeric(1001)
+fpr.vec = numeric(1001)
+for (i in 1:1001){
   s <- score.vec[i]
   true.positive <- sum((fitvalues > s) * clean.pima$test)
   false.positive <- sum((fitvalues > s) * (1 - clean.pima$test))
@@ -26,7 +43,25 @@ for (i in 1:101){
   false.negative <- sum((fitvalues <= s) * clean.pima$test)
   tpr.vec[i] <- true.positive/(true.positive + false.negative)
   fpr.vec[i] <- false.positive/(true.negative + false.positive)
-  }
-plot(score.vec,tpr.vec-fpr.vec,type = 'l',xlab = 'threshold', ylab = 'Sensitivty plus Specificity')
-plot(fpr.vec,tpr.vec,xlab = 'false positive rate',ylab = 'true positive rate',type = 'l')
+}
+
+# Output Plot of Sensitivity and Selectivity vs varuous thresholds to png
+png('Threshold.png')
+plot(score.vec,tpr.vec-fpr.vec,type = 'l',xlab = 'threshold', ylab = 'Sensitivty plus Specificity',main = 'Accuracy at Various Thresholds')
+dev.off()
+
+# Output ROC plot to png
+png('ROC.png')
+plot(fpr.vec,tpr.vec,xlab = 'false positive rate',ylab = 'true positive rate',type = 'l',col='red',main = 'ROC Plot')
 abline(0,1)
+dev.off()
+# Compute AUC
+pred = prediction(predict(log3,type = 'response'),clean.pima$test)
+perf = performance(pred,'auc')
+as.numeric(perf@y.values)
+
+# Make confusion matrix
+confusionMatrix(as.integer(predict(log3,type='response')>0.3),clean.pima$test)
+
+# Compute odds
+exp(coef(log3))
